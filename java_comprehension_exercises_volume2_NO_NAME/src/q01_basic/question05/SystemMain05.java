315c455547f8a0be2c1469425457c43b1e899775
megusato <megusato@C010422716.zipangu.cac.co.jp>
2026-04-30 13:05:48 +0900

    0430　q01_basic.question06まで完了 理解できた点 ・問題文を見たときに、どんなメソッドが必要なのか思い浮かぶようになってきた。
    不明な点、調査中
    ・q01_basic.question07の後半で苦戦している。

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/SystemMain05.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/SystemMain05.java
@@ -1,15 +1,13 @@
 package q01_basic.question05;
 
-import q01_basic.question05.AbstMember.Member;
-import q01_basic.question05.AbstMember.MemberManager;
-
 public class SystemMain05 {
 
 	public static void main(String[] args) {
 
 		AbstMember[] members = new AbstMember[2];
 		//TODO ここから実装する
-		members[0] = new Member("Sato Kensuke");
+
+		members[0] = new NonMember("Sato Kensuke");
 		members[1] = new Member(1, "Passw0rd", "Miura Manabu", 28, 2);
 
 		MemberManager.showAllMembers(members);
